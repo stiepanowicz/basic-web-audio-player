b210e4911fd2959f49ebc197fec15ef65c34330c
air-carbon <stiepanbogdan@gmail.com>
2023-08-28 17:40:49 +0200

    first commit

--- player.js
+++ player.js
@@ -1,9 +1,12 @@
 {
     class AudioPlayer extends HTMLElement {
+        playing = false;
+        currentTime = 0;
+        duration = 0;
+        
         constructor() {
             super();
     
-            this.playing = false;
             this.attachShadow({ mode: 'open' });
             this.render();
             this.initializeAudio();
@@ -20,6 +23,23 @@
 
         attachEvents() {
             this.playPauseBtn.addEventListener( 'click', this.togglePlay.bind(this) )
+
+            this.audio.addEventListener('loadedmetadata', () => {
+                this.duration = this.audio.duration;
+                this.progressBar.max = this.duration;
+
+                const secs = `${parseInt(`${this.duration % 60}`, 10)}`.padStart(2, '0');
+                const mins = parseInt(`${this.duration/60 % 60 }`, 10);
+
+                this.durationEl.textContent = `${mins}:${secs}`;
+
+                console.log('duration', this.audio.duration); 
+                console.log('currentTime', this.audio.currentTime);
+            })
+
+            this.audio.addEventListener('timeupdate', () => {
+                this.updateAudioTime(this.audio.currentTime);
+            })
         }
 
         async togglePlay() {
@@ -38,14 +58,35 @@
             }
         }
 
+        updateAudioTime(time) {
+            this.currentTime = time;
+            this.progressBar.value = this.currentTime;
+
+            const secs = `${parseInt(`${time % 60}`, 10)}`.padStart(2, '0');
+            const mins = parseInt(`${time/60 % 60 }`, 10);
+
+            this.currentTimeEl.textContent = `${mins}:${secs}`;
+        }
+
         render() {
+            // hide default player with style="display: none" on audio
             this.shadowRoot.innerHTML = `
+
             <audio src="Sunology, Pt. 2.mp3" controls></audio>
             <button class="play-btn" type="button">play</button>
+            <div class="progress-indicator">
+                <span class="current-time">0:00</span>
+                <input type="range" max="100" value="0" class="progress-bar">
+                <span class="duration">0:00</span>
+            </div>
             `;
 
             this.audio = this.shadowRoot.querySelector('audio');
             this.playPauseBtn = this.shadowRoot.querySelector('.play-btn');
+            this.progressIndicator = this.shadowRoot.querySelector('.progress-indicator');
+            this.currentTimeEl = this.progressIndicator.children[0];
+            this.progressBar = this.progressIndicator.children[1];
+            this.durationEl = this.progressIndicator.children[2];
         }
     }
     
